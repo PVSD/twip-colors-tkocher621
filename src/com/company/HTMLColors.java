0f6414932e71e9de1271d75e13263b4605c11b00
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2019-01-31 19:28:47 -0500

    Create HTMLColors.java

---
+++ src/com/company/HTMLColors.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class HTMLColors {
+
+    public int id;
+    public String name;
+    public String hex;
+
+    public HTMLColors(int id, String name, String hex)
+    {
+        this.id = id;
+        this.name = name;
+        this.hex = hex;
+    }
+
+}
